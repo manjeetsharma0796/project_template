f47a5ae9fc21fa89a68290e27ed655c824b241c1
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-04-12 11:57:33 +0530

    Created a script that creates new project template

---
+++ new_project_template.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+#mkdir ./src ./test ./lib 
+#touch ./src/${1}.js
+#touch ./test/${1}-test.js
+#cp ~/workspace/JS/vending-machine/lib/testing.js ./lib/testing.js
+
+cp -r ~/bin/project/template-sample/demo .
